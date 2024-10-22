2573ae200988112182bd9f0408cc8ce1da5ef2ac
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-10-23 00:38:14 +0100

    add support for using odoo's forms in external websites as iframes

---
+++ easy_iframe/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Easy iFrame',
+    'version': '18.0.0.0.1',
+    'license': 'LGPL-3',
+
+    'description': 'Provides support to use Odoo\'s webforms in external websites via iframes',
+    'author': 'Estudio Hawara <estudio@hawara.es>',
+    'category': 'Administration',
+
+    'depends': ['website'],
+
+    'data': [
+        'views/website_iframe.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
